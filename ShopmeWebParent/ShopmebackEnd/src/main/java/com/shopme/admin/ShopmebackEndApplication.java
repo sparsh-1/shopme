eb81f550266a77c87c59eb89782376bd826ff2e6
sparsh <sparshgpt18@gmail.com>
2024-05-19 11:16:31 +0530

    Initial set-up of Shopme project - Included bits of admin frontend UI

---
+++ ShopmeWebParent/ShopmebackEnd/src/main/java/com/shopme/admin/ShopmebackEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmebackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopmebackEndApplication.class, args);
+    }
+
+}
